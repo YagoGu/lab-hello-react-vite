6a24bf7cb5d5a13b6d3ce575f1d7c1f7fdd55260
YagoGu <yago.gutierrez15@gmail.com>
2023-08-21 13:21:36 +0200

    Responsive

--- hello-react/src/components/Section2.jsx
+++ hello-react/src/components/Section2.jsx
@@ -6,22 +6,22 @@ import icon4 from "./../assets/icon4.png";
 function Section2() {
     return (
         <section id="gallery">
-            <div>
+            <div class="compend">
                 <img src={icon1} alt="icon1" />
                 <p class="title">Declarative</p>
                 <p class="desc">React makes it painless to create interactive UIs</p>
             </div>
-            <div>
+            <div class="compend">
                 <img src={icon2} alt="icon2" />
                 <p class="title">Components</p>
                 <p class="desc">Build encapsulated components that manage their state.</p>
             </div>
-            <div>
+            <div class="compend">
                 <img src={icon3} alt="icon3" />
                 <p class="title">Single-Way</p>
                 <p class="desc">A set of immutable values are passed to the component's.</p>
             </div>
-            <div>
+            <div class="compend">
                 <img src={icon4} alt="icon4" />
                 <p class="title">JSX</p>
                 <p class="desc">Statically-typed, designed to run on modern browsers.</p>
